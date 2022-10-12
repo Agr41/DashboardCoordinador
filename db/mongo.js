48651fe56426765c32e4bb46914bcd97cf60b171
Mauricio Palomares <mau.palo2@gmail.com>
2022-10-12 17:31:33 -0500

    Crear archivos login

---
+++ db/mongo.js
@@ -0,0 +1,10 @@
+const { MongoClient } = require('mongodb');
+
+const url = 'mongodb+srv://pruebas:admin@clustergeneral.l4wbj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+//const url = 'mongodb://localhost:27018';
+const client = new MongoClient(url);
+
+
+const dbName = 'Alumnos';
+
+module.exports = {client, dbName}; 
---
+++ routes/uploads.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+const fileUpload = require('express-fileupload');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('index', { title: 'Express' });
+  }); 
+
+router.post('/upload', function(req, res) {
+    let sampleFile;
+    let uploadPath;
+  
+    if (!req.files || Object.keys(req.files).length === 0) {
+      return res.status(400).send('No files were uploaded.');
+    }
+  
+    // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+    sampleFile = req.files.sampleFile;
+    uploadPath = __dirname + '/uploads/' + sampleFile.name;
+  
+    // Use the mv() method to place the file somewhere on your server
+    sampleFile.mv(uploadPath, function(err) {
+      if (err)
+        return res.status(500).send(err);
+  
+      res.send('File uploaded!');
+    });
+  });
