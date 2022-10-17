b14bf21081190fe05b7e98432ccbf4eb20c41518
Mauricio Palomares <mau.palo2@gmail.com>
2022-10-17 15:53:55 -0500

    Crear archivos js

---
+++ routes/materias.js
@@ -0,0 +1,52 @@
+var express = require('express');
+var router = express.Router();
+var passport = require('passport');
+var {client,dbName} = require('../db/mongo');
+
+passport.deserializeUser(
+  async function(id, done) {
+    await client.connect();
+    const db = client.db(dbName);
+    const collection = db.collection('usuarios');
+    collection.findOne({username:id}, function (err, user) {
+      done(err, user);});
+});
+
+async function detalleUsu(id){
+  await client.connect();
+      const db = client.db(dbName);
+      const collection = db.collection('materias');
+      //let arregloMat = await collection.aggregate([{$match:{student_id:id}}]).toArray();
+      
+      
+      var dato = {}
+      console.log(dato)
+      return dato;
+  };
+
+/* GET home page. */
+router.get('/',(req, res, next) => {
+  if (req.isAuthenticated()) {
+      return next();
+  } else {
+      res.redirect('/login')
+  }
+}, function(req, res, next) {
+
+          //res.render('index', { title: "Menú Principal", student_id:req.user.student_id});
+          detalleUsu(req.user.username)
+          .then((dato)=>{
+            console.log(dato.promedio)
+            res.render('materias', { title: "Materias"});
+          })  
+          .catch((err)=>{
+              console.log(err);
+          })
+          .finally(()=>{
+              client.close
+          })
+
+  
+});
+
+module.exports = router;
