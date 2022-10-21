6713c615f14ff910ec686e7a469deb675a6ff097
Mauricio Palomares <mau.palo2@gmail.com>
2022-10-21 15:52:12 -0500

    Funcionalidad crear maestros

--- routes/crear_maestros.js
+++ routes/crear_maestros.js
@@ -1,52 +1,107 @@
 var express = require('express');
 var router = express.Router();
+var {client,dbName} = require('../db/mongo');
+var passport = require('passport');
+var LocalStrategy = require('passport-local');
+//var schema = require('../public/javascript/validation');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const Joi = require('joi');
+
+const schema = Joi.object({
+    correo: Joi.string()
+    .min(7)
+    .required(),
+
+    password: Joi.string()
+    .min(6)
+    .required(),
+    nombre: Joi.string()
+    .min(2)
+    .required(),
+    coordi: Joi.string()
+    .min(0),
+    active: Joi.boolean()
+    .required()
+    
+});
+
+
+
+
+
 var passport = require('passport');
 var {client,dbName} = require('../db/mongo');
 
 passport.deserializeUser(
-  async function(id, done) {
+    async function(id, done) {
+      await client.connect();
+      const db = client.db(dbName);
+      const collection = db.collection('usuarios');
+      collection.findOne({username:id}, function (err, user) {
+        done(err, user);
+  });
+});
+
+
+
+  router.get('/',(req, res, next) => {
+    if (req.isAuthenticated()) {
+        return next();
+    } else {
+        res.redirect('/')
+    }
+  }, function(req, res, next) {
+    res.render('crear_maestros', {title: "Crear maestros"});
+  });
+ 
+
+  router.post('/registro', async function(req, res, next){
+    try{
+    var value = await schema.validateAsync(req.body);
+    console.log(value);
+    //if (value = {}){
+    //}else{
+    regUser(value)
+      .then(()=>{
+        //AÑADIR MENSAJE DE ÉXITO DESPUÉS
+        res.send(`<script>alert("Registro exitoso")
+        window.location.href='/ver_maestros';
+        </script>`);
+        console.log("Registro correcto");
+      })
+      .catch((err)=>{
+        
+        console.log(err);
+        
+      })
+      .finally(()=>{
+        client.close()
+      })
+    }
+    catch (err) { 
+      res.send(`<script>alert("Por favor complete todos los campos")
+        window.location.href='/crear_maestros';
+        </script>`), console.log(err); }    
+    //}
+  });
+
+  async function regUser(datos){
     await client.connect();
+    console.log('Connected successfully to server');
     const db = client.db(dbName);
     const collection = db.collection('usuarios');
-    collection.findOne({username:id}, function (err, user) {
-      done(err, user);});
-});
-
-async function detalleUsu(id){
-  await client.connect();
-      const db = client.db(dbName);
-      const collection = db.collection('materias');
-      //let arregloMat = await collection.aggregate([{$match:{student_id:id}}]).toArray();
-      
-      
-      var dato = {}
-      console.log(dato)
-      return dato;
-  };
-
-/* GET home page. */
-router.get('/',(req, res, next) => {
-  if (req.isAuthenticated()) {
-      return next();
-  } else {
-      res.redirect('/login')
+    var hash = await bcrypt.hash(datos.password, saltRounds);
+    console.log(hash + "externo");
+    await collection.insertOne(
+        {
+          username: datos.correo,
+          password: hash,
+          nombre:datos.nombre,
+          coordi: Boolean(datos.coordi),
+          active:datos.active
+        }
+      );
+      console.log(datos.usuario); 
   }
-}, function(req, res, next) {
-
-          //res.render('index', { title: "Menú Principal", student_id:req.user.student_id});
-          detalleUsu(req.user.username)
-          .then((dato)=>{
-            console.log(dato.promedio)
-            res.render('crear_maestros', { title: "Crear maestros"});
-          })  
-          .catch((err)=>{
-              console.log(err);
-          })
-          .finally(()=>{
-              client.close
-          })
-
-  
-});
-
-module.exports = router;
+    module.exports = router;
